22ed5bfbd6de00dc2125ced3dba0fe3a14cced38
Luigi <luigi.isidori@yahoo.it>
2020-09-05 12:58:42 +0200

    added management for the icon in weather component with climate model, completed STEP1

--- src/app/component/weather/weather.component.ts
+++ src/app/component/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {OpenWeatherMapModel} from "../../model/open-weather-map-model";
 import {OpenWeatherMapResponse} from "../../model/open-weather-map-response";
+import {Climate} from "../../model/climate";
 
 @Component({
     selector: 'app-weather',
@@ -11,14 +12,41 @@ export class WeatherComponent implements OnInit {
 
     @Input() weather: OpenWeatherMapResponse;
     @Output() removeEvent = new EventEmitter<string>();
+    imgUrl: string= 'https://www.angulartraining.com/images/weather';
+    currClimate: string;
 
     constructor() { }
 
     ngOnInit(): void {
-        console.log(this.weather);
+        this.getImgUrl();
     }
 
     closeBox(){
         this.removeEvent.emit();
+    }
+
+    private getImgUrl(){
+
+        this.currClimate = this.weather.responseBody.weather[0].main;
+
+        switch (this.currClimate){
+            case Climate.Clear:{
+                this.imgUrl += '/sun.png'
+                break;
+            }
+            case Climate.Clouds:{
+                this.imgUrl += '/clouds.png';
+                break;
+            }
+            case Climate.Rain:{
+                this.imgUrl += '/rain.png';
+                break;
+            }
+            case Climate.Snow:{
+                this.imgUrl += '/snow.png'
+                break;
+            }
+        }
+
     }
 }
---
+++ src/app/model/climate.ts
@@ -0,0 +1,9 @@
+
+export enum Climate{
+
+    Clear= 'Clear',
+    Rain= 'Rain',
+    Clouds= 'Clouds',
+    Snow= 'Snow'
+
+}
